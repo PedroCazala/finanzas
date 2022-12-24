932f6860be32ac42018293205fcfa3c38985f77f
Pedro Cazala <cazalapedro@gmail.com>
2022-12-24 02:09:17 -0300

    se comenzó con el aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "bootswatch/dist/cyborg/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "./App.scss";
+import Aside from "./components/Aside/Aside";
 import CountCash from "./components/pages/CountCash/CountCash";
 
 function App() {
     return (
         <div className="App">
             <Router>
-                <h1>hola</h1>
-                hola mundo
+                <Aside/>
                 <Routes>
                     <Route path="/" element={<p>Este es el index</p>} />
                     <Route path="/contar-efectivo" element={<CountCash/>} />
---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './Aside.scss'
+
+function Aside() {
+    return (
+        <aside>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+
+                <NavLink to="/" className="navbar-brand" >HOME</NavLink>
+                <NavLink to="/contar-efectivo" className="navbar-brand" > Contar Efectivo</NavLink>
+            </div>
+            </nav>
+        </aside>
+    );
+}
+
+export default Aside;
